6d8aa2dffeae33ed1f6bf329e68221dd54d67ecd
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-04-26 10:33:11 +0300

    Handle PROTECT,DO_NOTHING relations upon deleation

--- related_objects_fetcher.py
+++ related_objects_fetcher.py
@@ -1,6 +1,6 @@
 from django.db.models import Model
 from django.core.paginator import Paginator
-from django.db.models.deletion import CASCADE,SET_NULL
+from django.db.models.deletion import CASCADE,SET_NULL,DO_NOTHING,PROTECT
 
 class PaginatorObject:
     """ This class only for returning a list off objects insted of KEY:VALUE dict to the paginator  """
@@ -41,17 +41,35 @@ class RelatedObjectsCollector:
     
     def __collect_objects(self):
         objects = {}
+        self.can_be_deleted = True
+        self.reason = ''
         for relation in self.related_objects:
             # Get relation metadata
             on_delete = self.get_on_delete_action(relation)
             query_name = relation.get_accessor_name()
             related_model = relation.related_model
+            
+            if on_delete == 'protect':
+                self.can_be_deleted = False
+                self.reason = (
+                    f'The related model <strong>{related_model._meta.model_name}</strong> '
+                    f'has a relationship with the model <strong>{self.instance._meta.model_name}</strong>, '
+                    f'and this relationship is set to <strong>PROTECT</strong>, '
+                    f'which prevents the deletion of this instance.'
+                )
+                return 
             # Only apply the collectation of related objects if the related_model has a relationship of ForeignKey,OneToOne fields 
             if relation.one_to_many or relation.one_to_one:
                 # Only collect data that will be deleted or set_null for the instance field 
-                if on_delete == "delete" or on_delete == "set_null":
+                if on_delete == "delete" or on_delete == "set_null" or on_delete == 'do_nothing' or on_delete == 'protect':
                     if hasattr(self.instance,query_name):
                         query = list(getattr(self.instance,query_name).all())
+                        
+                        if len(query) and on_delete == 'do_nothing':
+                            self.can_be_deleted = False
+                            self.reason = f"The instance of <strong>{self.instance._meta.model_name}</strong> you are trying to delete has existing related instances in <strong>{related_model._meta.model_name}</strong>. The relationship between these models is set to <strong>DO_NOTHING</strong>, which means that no action is taken on the related objects when the parent instance is deleted. This will raise an error because the related instances still depend on the deleted object."
+                            return
+                        
                         if not related_model in objects:
                             if query:
                                 objects[related_model]  = {
@@ -83,6 +101,12 @@ class RelatedObjectsCollector:
         
         elif relation.on_delete == SET_NULL:
             return "set_null"
+        
+        elif relation.on_delete == DO_NOTHING:
+            return 'do_nothing'
+        
+        elif relation.on_delete == PROTECT:
+            return "protect"
         
         return "unknown"
 
